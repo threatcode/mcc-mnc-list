75ed614d5f766d463ea1a8a7b8c551b8ef52368f
gitworkflows <118260833+gitworkflows@users.noreply.github.com>
2025-02-17 23:46:51 +0600

    Create status-codes.py

---
+++ status-codes.py
@@ -0,0 +1,15 @@
+status_codes = [
+    "Allocated",
+    "Implement / Design",
+    "Not operational",
+    "Ongoing",
+    "Operational",
+    "Planned",
+    "Reserved",
+    "Returned spare",
+    "Temporary operational",
+    "Test Network",
+    "Testing",
+    "Unknown",
+    "Upcoming"
+]
